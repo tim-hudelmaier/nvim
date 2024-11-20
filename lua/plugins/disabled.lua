48d75b928c03e8403d699e6e8ce4ca6dc0eead43
tim-hudelmaier <tim.hudelmaier@stud.uni-heidelberg.de>
2024-11-20 17:21:38 +0100

    update config

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,5 @@
+-- home for all my disabled plugins
+
+return {
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+}
---
+++ lua/plugins/extend-nvim-cmp.lua
@@ -0,0 +1,26 @@
+return {
+  "hrsh7th/nvim-cmp",
+  opts = function(_, opts)
+    opts.mapping["<CR>"] = nil
+
+    local cmp = require("cmp")
+
+    opts.mapping = vim.tbl_extend("force", opts.mapping, {
+      ["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
+          cmp.confirm({ select = true })
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+      ["<S-Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+    })
+  end,
+}
